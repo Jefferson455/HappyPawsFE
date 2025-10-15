ce2b13f6067c195aa62c85a82a08fb8ed4525e57
Juliana Saldaña <juliana.saldana@expf.local>
2025-10-14 23:31:35 -0500

    install de primeNG con prueba de button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+
+import { FormsModule } from '@angular/forms';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    FormsModule, ToggleButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  checked: boolean = false;
   title = 'HappyPawsFE';
 }
